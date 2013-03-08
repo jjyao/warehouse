6227b26fd83fdbcbeb1d0bb95a830a5aeacc5fc8
姚嘉俊 <jeromeyjj@gmail.com>
2013-03-08 20:59:22 +0800

    Add ssh config file Update wordpress.sh

--- scripts/wordpress.sh
+++ scripts/wordpress.sh
@@ -5,29 +5,33 @@ MYSQL_ROOT_PASSWORD='password'
 if [ $# -ne 1 ]; then
     echo "Usage: sh wordpress.sh sitename"
 fi
+
+sitename=$1
+sanitized_sitename=${sitename//./_}
+
 # download wordpress
 wget http://wordpress.org/latest.tar.gz -O wordpress.tmp.tar.gz
 tar -xz -C /tmp -f wordpress.tmp.tar.gz
 rm -f wordpress.tmp.tar.gz
-mv /tmp/wordpress $1
+mv /tmp/wordpress $sitename
 
 # harden wordpress
-mkdir $1/wp-content/uploads
-find $1 -type d -exec chmod 750 {} \;
-find $1 -type f -exec chmod 640 {} \;
-chmod 770 $1/wp-content/uploads
+mkdir sitename/wp-content/uploads
+find sitename -type d -exec chmod 750 {} \;
+find sitename -type f -exec chmod 640 {} \;
+chmod 770 sitename/wp-content/uploads
 
 # config mysql
-mysql -f -u$MYSQL_ROOT_USERNAME -p$MYSQL_ROOT_PASSWORD -e <<EOSQL "CREATE DATABASE IF NOT EXISTS $1;
-GRANT ALL ON $1.* TO '$1'@'localhost' IDENTIFIED BY '$1';
+mysql -f -u$MYSQL_ROOT_USERNAME -p$MYSQL_ROOT_PASSWORD -e <<EOSQL "CREATE DATABASE IF NOT EXISTS $sanitized1;
+GRANT ALL ON $sanitized_sitename.* TO '$sanitized_sitename'@'localhost' IDENTIFIED BY '$sanitized_sitename';
 FLUSH PRIVILEGES;"
 EOSQL
 
 # edit wp-config.php
-mv $1/wp-config-sample.php $1/wp-config.php
-sed -i "s/^define('DB_NAME'.*);/define('DB_NAME', '$1');/g"  $1/wp-config.php
-sed -i "s/^define('DB_USER'.*);/define('DB_USER', '$1');/g"  $1/wp-config.php
-sed -i "s/^define('DB_PASSWORD'.*);/define('DB_PASSWORD', '$1');/g"  $1/wp-config.php
+mv sitename/wp-config-sample.php sitename/wp-config.php
+sed -i "s/^define('DB_NAME'.*);/define('DB_NAME', 'sitename');/g"  sitename/wp-config.php
+sed -i "s/^define('DB_USER'.*);/define('DB_USER', 'sitename');/g"  sitename/wp-config.php
+sed -i "s/^define('DB_PASSWORD'.*);/define('DB_PASSWORD', 'sitename');/g"  sitename/wp-config.php
 salts=`curl https://api.wordpress.org/secret-key/1.1/salt`
-printf '%s\n' "g/put your unique phrase here/d" a "$salts" . w | ed -s $1/wp-config.php
-sed -i "s/\r//g" $1/wp-config.php
+printf '%s\n' "g/put your unique phrase here/d" a "$salts" . w | ed -s sitename/wp-config.php
+sed -i "s/\r//g" sitename/wp-config.php
