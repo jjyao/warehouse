3045036ba7689b157601374b890528d07452f32b
姚嘉俊 <jeromeyjj@gmail.com>
2013-04-06 16:17:27 +0800

    Switch to Puppet

---
+++ configs/jjyao.pp
@@ -0,0 +1,40 @@
+# global config
+Package {
+    ensure => 'installed',
+}
+
+Exec {
+    path => ["/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin/"]
+}
+
+package { ['git-core', 'nautilus-open-terminal', 'shutter', 'python-pip', 'htop', 'nmon', 'exuberant-ctags', 'ack-grep']:
+}
+
+package { ['flake8']:
+    provider => 'pip',
+}
+
+exec { 'git':
+    command => 'git config --global user.name "姚嘉俊";
+                git config --global user.email "jeromeyjj@gmail.com";
+                git config --global alias.lg "log --color --graph --pretty=format:\'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset\' --abbrev-commit --";
+                git config --global alias.unstage "reset HEAD";
+                git config --global core.editor vim;
+                git config --global core.pager less;
+                git config --global color.ui true;
+                git config --global core.autocrlf input;',
+}
+
+exec { 'ssh':
+    command => 'ssh-keygen -t rsa -C "jeromeyjj@gmail.com"',
+}
+
+notify { 'ssh':
+    message => 'Add SSH key to GitHub and complete the installation of Git',
+}
+
+exec { 'neocomplcache':
+    command => 'git clone https://github.com/vim-scripts/neocomplcache.git /usr/share/vim/plugins/neocomplcache',
+}
+
+Package['git-core'] -> Exec['git'] -> Exec['ssh'] -> Notify['ssh'] -> Exec['neocomplcache'] 
